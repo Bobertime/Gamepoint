bdb3a92e29eb32518224c5e072f925d8ad1e551b
Bober <bobrerco.il@gamil.com>
2024-02-10 15:50:07 +0200

    Добавьте файлы проекта.

---
+++ Gamepoint/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Gamepoint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.boxPlayer = new System.Windows.Forms.PictureBox();
+            this.btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.player.Location = new System.Drawing.Point(320, 590);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(200, 30);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // boxPlayer
+            // 
+            this.boxPlayer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.boxPlayer.Location = new System.Drawing.Point(418, 388);
+            this.boxPlayer.Name = "boxPlayer";
+            this.boxPlayer.Size = new System.Drawing.Size(20, 20);
+            this.boxPlayer.TabIndex = 1;
+            this.boxPlayer.TabStop = false;
+            // 
+            // btn
+            // 
+            this.btn.BackColor = System.Drawing.Color.Red;
+            this.btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn.Font = new System.Drawing.Font("Arial Rounded MT Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn.ForeColor = System.Drawing.Color.White;
+            this.btn.Location = new System.Drawing.Point(320, 272);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(155, 47);
+            this.btn.TabIndex = 2;
+            this.btn.Text = "Restart";
+            this.btn.UseVisualStyleBackColor = false;
+            this.btn.Visible = false;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(840, 650);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.boxPlayer);
+            this.Controls.Add(this.player);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox boxPlayer;
+        private System.Windows.Forms.Button btn;
+    }
+}
+
---
+++ Gamepoint/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gamepoint
+{
+    public partial class Form1 : Form
+    {
+        private int speed = 5;
+        private string direction = "Down";
+        private bool lose = false;
+        private List<PictureBox> pictureBoxList = new List<PictureBox>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            CreatePictureBox();
+            KeyPreview = true;
+        }
+
+        private void CreatePictureBox()
+        {
+            for (int i = 0; i < 33; i++)
+            {
+                PictureBox pictureBox = new PictureBox();
+                pictureBox.Location = new Point(10 + i * 25, 10); // Устанавливаем координаты расположения
+                pictureBox.Size = new Size(20, 20); // Устанавливаем размер
+                pictureBox.BackColor = Color.FromArgb(255, 128, 0);
+                pictureBoxList.Add(pictureBox);
+                Controls.Add(pictureBox); // Добавляем PictureBox на форму
+            }
+        }
+
+        private void BoolBoundPictureBoxwithPlayer()
+        {
+            for (int i = 0; i < pictureBoxList.Count; i++)
+            {
+                if (pictureBoxList[i].Bounds.IntersectsWith(boxPlayer.Bounds))
+                {
+                    pictureBoxList[i].Dispose();
+                    if (direction == "UpLeft")
+                    {
+                        direction = "DownLeft";
+                    } else if (direction == "UpRight")
+                    {
+                        direction = "DownRight";
+                    }
+                }
+            }
+        }
+
+        private void MoveDirection()
+        {
+            switch (direction) 
+            {
+                case "UpLeft":
+                    boxPlayer.Left -= speed;
+                    boxPlayer.Top -= speed;
+                    break;
+                case "UpRight":
+                    boxPlayer.Left += speed;
+                    boxPlayer.Top -= speed;
+                    break;
+                case "DownLeft":
+                    boxPlayer.Left -= speed;
+                    boxPlayer.Top += speed;
+                    break;
+                case "DownRight":
+                    boxPlayer.Left += speed;
+                    boxPlayer.Top += speed;
+                    break;
+                case "Down":
+                    boxPlayer.Top += speed;
+                    break;
+            }
+        }
+
+        private void BoolBoundsBoxPlayer()
+        {
+            if ((boxPlayer.Bounds.Right >= 840) && direction == "UpRight") 
+            {
+                direction = "UpLeft";
+            }
+            if ((boxPlayer.Bounds.Right >= 840) && direction == "DownRight")
+            {
+                direction = "DownLeft";
+            }
+            if ((boxPlayer.Bounds.Top <= 0) && direction == "UpRight")
+            {
+                direction = "DownRight";
+            }
+            if ((boxPlayer.Bounds.Top <= 0) && direction == "UpLeft")
+            {
+                direction = "DownLeft";
+            }
+            if ((boxPlayer.Bounds.Left <= 0) && direction == "UpLeft")
+            {
+                direction = "UpRight";
+            }
+            if ((boxPlayer.Bounds.Left <= 0) && direction == "DownLeft")
+            {
+                direction = "DownRight";
+            }
+            if (boxPlayer.Bounds.Bottom >= 650)
+            {
+                timer.Enabled = false;
+                btn.Visible = true;
+                lose = true;
+            }
+        }
+
+        private void BoolBoundsBoxPlayerWithPlayer()
+        {
+            switch (boxPlayer.Bounds.IntersectsWith(player.Bounds))
+            {
+                case true:
+                    if (direction == "Down")
+                    {
+                        Random rand = new Random();
+                        int nav = rand.Next(-5, 6);
+                        if (nav >= 0)
+                        {
+                            direction = "UpRight";
+                        }
+                        else if (nav < 0)
+                        {
+                            direction = "UpLeft";
+                        }
+                    }
+                    if (direction == "DownLeft")
+                    {
+                        direction = "UpLeft";
+                    }
+                    else
+                    {
+                        direction = "UpRight";
+                    }
+                    break;
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            MoveDirection();
+            BoolBoundsBoxPlayer();
+            BoolBoundsBoxPlayerWithPlayer();
+            BoolBoundPictureBoxwithPlayer();
+
+
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (lose)
+            {
+                return;
+            }
+            int speed = 10;
+            if ((e.KeyCode == Keys.Left || e.KeyCode == Keys.A) && player.Left >= 0)
+            {
+                player.Left -= speed;
+            } else if ((e.KeyCode == Keys.Right || e.KeyCode == Keys.D) && player.Right <= 840) 
+            {
+                player.Left += speed;
+            }
+        }
+        private void btn_Click(object sender, EventArgs e)
+        {
+            direction = "Down";
+            boxPlayer.Left = 388;
+            boxPlayer.Top = 418;
+            player.Left = 320;
+            timer.Enabled = true;
+            btn.Visible = false;
+            lose = false;
+        }
+
+
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Escape) { Close(); }
+        }
+
+        
+    }
+}
